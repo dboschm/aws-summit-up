0c8d1c13564230e0b7ceb2e3937b3911ccaa408b
David Boschmann <davidboschmann@live.de>
2024-05-15 23:13:34 +0200

    feat: log body

--- service/lambda_handler.py
+++ service/lambda_handler.py
@@ -3,7 +3,7 @@ import aws_lambda_powertools.event_handler
 import aws_lambda_powertools.shared
 import aws_lambda_powertools.shared.types
 import aws_lambda_powertools.utilities
-from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
+from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent, APIGatewayProxyEventV2
 from aws_lambda_powertools.utilities.typing import LambdaContext
 import boto3
 import aws_lambda_powertools
@@ -11,8 +11,8 @@ import aws_lambda_powertools
 # Annahme, der Tischname heißt MyTable
 table = boto3.resource('dynamodb').Table('MyTable')
 
-def handler(event: APIGatewayProxyEvent, context: LambdaContext):
-
+def handler(event, context):
+    event = APIGatewayProxyEventV2(event)
     print(event.body)
     return {
         'statusCode': 200,
